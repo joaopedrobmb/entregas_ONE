2a58339ef75d48d79d9895bd4890987cb66897c5
joaopedrobmb <joaopedrobmb@gmail.com>
2025-03-13 17:33:06 -0300

    Desafio Amigo Secreto

--- challenge_amigo_secreto/app.js
+++ challenge_amigo_secreto/app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+    const amigo = document.getElementById('amigo').value;
+
+    if (amigo == '') {
+        alert('Por favor, insira um nome.')
+    } else {
+        amigos.push(amigo)
+        document.getElementById('amigo').value = ''
+    }
+
+    const listaAmigos = document.getElementById('listaAmigos');
+
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);        
+    })};
+
+function sortearAmigo(){
+
+    if (amigos.length == 0){
+        alert('Por favor, adicione amigos para sortear.')
+    } else {
+        let numeroSorteado = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[numeroSorteado]
+
+        const resultadoCampo = document.getElementById('resultado');
+        
+        resultadoCampo.textContent = amigoSorteado;
+        resultadoCampo.innerHTML = amigoSorteado;
+    }
+}
